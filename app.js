e418e7c0e6d20e3bff5919f0fa3060d8d564eece
ronit <uttkarshbartwal@gmail.com>
2025-02-27 09:18:06 +0530

    pipeline of this work

--- app.js
+++ app.js
@@ -15,10 +15,9 @@ app.get('/version', (req, res) => {
 })
 
 app.get('/health', (req, res) => {
-    // eslint-disable-next-line no-constant-condition
-    if (true) throw('error...  ')
     res.send('ok')
 })
+
 
 app.listen(PORT, () => {
     console.log(`server started on port ${PORT}`)
